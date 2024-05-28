2af115b5948d8894a4e6dc7948bf36f43037d100
Ahmad Saleh <ahmadsaleh@quark.dev>
2024-05-28 15:10:33 +0200

    use latest client sdk

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }
     }
 }
 
